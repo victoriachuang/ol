8b4222d474d26271ffcace182d8966626e4e48d2
victoriachuang <viiictoriachuang@gmail.com>
2016-05-05 21:35:45 -0500

    download businesses csv write each row to json file

--- app.rb
+++ app.rb
@@ -1,17 +1,22 @@
-require 'sinatra.rb'
+# require 'sinatra'
+require 'csv'
+require 'json'
 
-get '/' do
-	'Hello world'
+# get '/' do
+# 	'Hello world'
+# end
+
+# get '/businesses/:id' do
+
+# end
+
+file = File.open('businesses.json', 'w')
+
+businesses = CSV.open('engineering_project_businesses.csv', :headers => true)
+
+businesses.each do |row|
+	file.write(row.to_hash.to_json)
+	file.write("\n")
 end
 
-post '/' do
-
-end
-
-put '/' do
-
-end
-
-delete '/' do
-
-end
+file.close
