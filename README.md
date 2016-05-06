7b8f11444e7a6a8d15d0f1a65e62ff78e03d89c9
victoriachuang <viiictoriachuang@gmail.com>
2016-05-05 22:17:40 -0500

    add todos

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ol
+
+### Todo's as of May 5
+* Create objects from each CSV row
+* Feed data as JSON
--- app.rb
+++ app.rb
@@ -1,22 +1,25 @@
-# require 'sinatra'
+require 'sinatra'
 require 'csv'
 require 'json'
 
-# get '/' do
-# 	'Hello world'
+# write each row of data to json file
+# file = File.open('businesses.json', 'w')
+# businesses = CSV.open('engineering_project_businesses.csv', :headers => true)
+# businesses.each do |row|
+# 	file.write(row.to_hash.to_json)
+# 	file.write("\n")
+# end
+# file.close
+
+# get '/businesses' do
+
 # end
 
 # get '/businesses/:id' do
 
 # end
 
-file = File.open('businesses.json', 'w')
-
-businesses = CSV.open('engineering_project_businesses.csv', :headers => true)
-
-businesses.each do |row|
-	file.write(row.to_hash.to_json)
-	file.write("\n")
-end
-
-file.close
+# open("http://google.com") do |f|
+# 	puts f.base_uri
+# 	puts f.status
+# end
