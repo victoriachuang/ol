578d5e56f0189cd289a34ba997076908acb9996d
victoriachuang <viiictoriachuang@gmail.com>
2016-05-06 18:58:17 -0500

    able to read csv and create objects with corresponding properties

--- app.rb
+++ app.rb
@@ -1,15 +1,43 @@
-require 'sinatra'
+# require 'rubygems'
+# require 'sinatra'
 require 'csv'
-require 'json'
+# require 'json'
+# require 'data_mapper'
+# require 'sqlite'
 
-# write each row of data to json file
-# file = File.open('businesses.json', 'w')
-# businesses = CSV.open('engineering_project_businesses.csv', :headers => true)
-# businesses.each do |row|
-# 	file.write(row.to_hash.to_json)
-# 	file.write("\n")
+# DataMapper.setup(:default, 'sqlite::memory:')
+
+class Business
+
+	attr_reader :id, :uuid, :name, :address, :address2, :city, :state, :zip, :country, :phone, :website, :created_at
+
+	def initialize(id, uuid, name, address, address2, city, state, zip, country, phone, website, created_at)
+		@id = id
+		@uuid = uuid
+		@name = name
+		@address = address
+		@address2 = address2
+		@city = city
+		@state = state
+		@zip = zip
+		@country = country
+		@phone = phone
+		@website = website
+		@created_at = created_at
+	end
+end
+
+businesses = CSV.read("./test.csv")
+
+businesses.each do |property|
+	hello = Business.new(property[0], property[1], property[2], property[3], property[4], property[5], property[6], property[7], property[8], property[9], property[10], property[11])
+  # p hello
+end
+
+# CSV.read("test.csv") do |row|
+# 	data = row.split(',')
+#   
 # end
-# file.close
 
 # get '/businesses' do
 
@@ -17,9 +45,4 @@ require 'json'
 
 # get '/businesses/:id' do
 
-# end
-
-# open("http://google.com") do |f|
-# 	puts f.base_uri
-# 	puts f.status
 # end
