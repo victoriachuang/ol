f90af76bb9f5711a7aa2b5818ac384b09e0d8e2c
victoriachuang <viiictoriachuang@gmail.com>
2016-05-07 11:30:58 -0500

    clean up comments

--- app.rb
+++ app.rb
@@ -1,14 +1,10 @@
-# require 'rubygems'
 require 'sinatra'
 require 'csv'
 require 'json'
-# require 'data_mapper'
-# require 'sqlite'
-
-# DataMapper.setup(:default, 'sqlite::memory:')
 
 class Business
 
+	# read each attribute
 	attr_reader :id, :uuid, :name, :address, :address2, :city, :state, :zip, :country, :phone, :website, :created_at
 
 	def initialize(id, uuid, name, address, address2, city, state, zip, country, phone, website, created_at)
@@ -26,6 +22,7 @@ class Business
 		@created_at = created_at
 	end
 
+	# custom to_json method for created class
 	def to_json
 		{'id' => @id,
 			'uuid' => @uuid,
@@ -46,18 +43,15 @@ end
 businesses = CSV.read("./test.csv")
 $all_businesses = []
 
+# create Business object from each line of CSV
 businesses.each do |property|
 	$all_businesses << Business.new(property[0], property[1], property[2], property[3], property[4], property[5], property[6], property[7], property[8], property[9], property[10], property[11])
-  # p hello
 end
 
 get '/businesses' do
-	# $all_businesses.each do |b|
-	# 	b.to_json
-	# end
-	sample = $all_businesses[1]
-	JSON.pretty_generate(sample.to_json)
-
+	$all_businesses.each do |b|
+		JSON.pretty_generate(b.to_json)
+	end
 end
 
 # get '/businesses/:id' do
