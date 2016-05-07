0c10370c021348e42fcbd969104b3c57a5fc939d
victoriachuang <viiictoriachuang@gmail.com>
2016-05-07 12:43:28 -0500

    able to render businesses as json, limit 50

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class Business
 	end
 
 	# custom to_json method for created class
-	def to_json
+	def to_json(*a)
 		{'id' => @id,
 			'uuid' => @uuid,
 			'name' => @name,
@@ -36,25 +36,37 @@ class Business
 			'phone' => @phone,
 			'website' => @website,
 			'created_at' => @created_at
-		}
+		}.to_json(*a)
 	end
+
+		# def self.all
+  #   	ObjectSpace.each_object(self).to_a
+  # 	end
+
+
+	# def find_by_id
+	# 	$all_businesses.
+	# end
 end
 
-businesses = CSV.read("./test.csv")
+businesses = CSV.read("./engineering_project_businesses.csv")
 $all_businesses = []
+
+# remove header row of CSV
+businesses.shift
 
 # create Business object from each line of CSV
 businesses.each do |property|
 	$all_businesses << Business.new(property[0], property[1], property[2], property[3], property[4], property[5], property[6], property[7], property[8], property[9], property[10], property[11])
 end
 
-get '/businesses' do
-	$all_businesses.each do |b|
-		JSON.pretty_generate(b.to_json)
-	end
+get '/businesses?:page' do
+
+	print_businesses = $all_businesses.slice(0, 50)
+	print_businesses.to_json
 end
 
 # get '/businesses/:id' do
-# 	b = Business.get(params[:id])
-# 	b.to_json
+# 	b = Business.find(1)
+# 	JSON.pretty_generate(b.to_json)
 # end
