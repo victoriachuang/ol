29c867f17adee288b595e8bd58af5494defb4719
victoriachuang <viiictoriachuang@gmail.com>
2016-05-10 16:17:57 -0500

    add test for business route with valid ID

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,8 +1,13 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
-describe "My Sinatra Application" do
-  it "allow access to businesses route" do
+describe "Businesses listings" do
+  it "allows access to businesses route" do
     get '/businesses'
     expect(last_response).to be_ok
+  end
+
+  it "allows access to business with valid ID" do
+  	get '/businesses/5'
+  	expect(last_response).to be_ok
   end
 end
