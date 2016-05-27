ba69eb2bc4c4d7ac55c8f09b1fb93a1199aa2737
ANDREW REUTTER <andrewreutter@ANDREWs-MacBook-Pro.local>
2016-05-27 09:55:58 -0500

    tests that fail until the server-side is implemented

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,28 +1,49 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
 describe 'Businesses listings' do
-  it 'allows access to businesses route' do
+
+  it 'denies access to businesses route if no token is provided' do
     get '/businesses'
+    expect(last_response.status).to eq(401)
+  end
+
+  it 'denies access to business route if no token is provided' do
+    get '/businesses/5'
+    expect(last_response.status).to eq(401)
+  end
+
+  it 'denies access to businesses route if a bad token is provided' do
+    bad_authenticated_get '/businesses'
+    expect(last_response.status).to eq(403)
+  end
+
+  it 'denies access to business route if no token is provided' do
+    bad_authenticated_get '/businesses/5'
+    expect(last_response.status).to eq(403)
+  end
+
+  it 'allows access to businesses route' do
+    authenticated_get '/businesses'
     expect(last_response).to be_ok
   end
 
   it 'allows access to business with valid ID' do
-  	get '/businesses/5'
+  	authenticated_get '/businesses/5'
   	expect(last_response).to be_ok
   end
 
   it 'raises an error when requesting an invalid page' do
-  	get '/businesses?page=60000'
+  	authenticated_get '/businesses?page=60000'
   	expect(last_response.status).to eq(404)
   end
 
   it 'raises an error when requesting an invalid business ID' do
-  	get '/businesses/60000'
+  	authenticated_get '/businesses/60000'
   	expect(last_response.status).to eq(404)
   end
 
   it 'redirects the route when path contains a leading forward slash' do
-  	get '/businesses/'
+  	authenticated_get '/businesses/'
   	expect(last_response.status).to eq(302)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,14 @@ require File.expand_path '../../app.rb', __FILE__
 module RSpecMixin
   include Rack::Test::Methods
   def app() Sinatra::Application end
+
+  def authenticated_get(uri)
+    get uri, nil, {'Authorization' => "lg_JhXDOSTZNJcVjH7IpsQ"}
+  end
+
+  def bad_authenticated_get(uri)
+    get uri, nil, {'Authorization' => "THIS_IS_NOT_A_REAL_TOKEN"}
+  end
 end
 
 RSpec.configure { |c| c.include RSpecMixin }
